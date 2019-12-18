74a64aaf4a27fd55e7ca2b0b0a7734b10c30328a
lukasso <lukasz.koladko@gmail.com>
2019-12-18 13:47:07 +0100

    fixed API auth

--- 9-twilio-challenge/explore.js
+++ 9-twilio-challenge/explore.js
@@ -1,8 +1,8 @@
- const Twilio = require("twilio");
-   const accountSid = 'ACe05b1b41bd9ae32013db4d1098198072';
-const authToken = '551568a4277dc5e583e15546e86e9937';
+const Twilio = require("twilio");
+const accountSid = process.env.accountSid;
+const authToken = process.env.authToken;
 const client = require('twilio')(accountSid, authToken);
-//  const client = new Twilio("ACe05b1b41bd9ae32013db4d1098198072", "551568a4277dc5e583e15546e86e9937");
+
  
  client.messages
   .list()
